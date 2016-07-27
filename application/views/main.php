0430c0a5a43fd7c1519d436bc116d75b5d90ca99
Janatbek <janatbek@gmail.com>
2016-07-27 12:17:43 -0700

    first commit with bootstrap in it

---
+++ application/views/main.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Product Page</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	
+</head>
+<body>
+
+<div class="container">
+	
+</div>
+
+</body>
+</html>
